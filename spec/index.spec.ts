14a3d11f8c2025d9a43b1a6e0597028f874c6002
Evgeny Yakimov <john.yakimov@gmail.com>
2024-12-30 08:44:30 +0000

    WIP

--- spec/index.spec.ts
+++ spec/index.spec.ts
@@ -1,5 +1,111 @@
-describe('test', () => {
-  it('test', () => {
-    expect(true).toBe(true);
+import { injectContentScripts } from '../src/index';
+
+export class ChromeMock {
+  runtime = {
+    getManifest: jasmine.createSpy('getManifest').and.returnValue({
+      host_permissions: ['https://*.example.com/*'],
+      content_scripts: [
+        {
+          matches: ['https://*.example.com/*'],
+          exclude_matches: ['https://excluded.example.com/*'],
+          js: ['content.js'],
+          css: ['styles.css'],
+        },
+      ],
+    }),
+  };
+  tabs = {
+    query: jasmine.createSpy('query').and.resolveTo([]),
+    get: jasmine.createSpy('get').and.resolveTo({}),
+  };
+  scripting = {
+    insertCSS: jasmine.createSpy('insertCSS').and.resolveTo(),
+    executeScript: jasmine.createSpy('executeScript').and.resolveTo(),
+  };
+}
+
+describe('With mocked chrome and service worker', () => {
+  let chromeMock: ChromeMock;
+
+  beforeEach(() => {
+    chromeMock = new ChromeMock();
+    globalThis.chrome = chromeMock as unknown as typeof chrome;
+    globalThis.addEventListener = jasmine.createSpy('addEventListener');
+  });
+
+  afterEach(() => {
+    delete globalThis['chrome' as keyof typeof globalThis];
+    delete globalThis['addEventListener' as keyof typeof globalThis];
+  });
+
+  describe('injectContentScripts', () => {
+    it('should not inject into unloaded tabs', async () => {
+      chromeMock.tabs.query
+        .withArgs({ url: 'https://*.example.com/*', status: 'complete' })
+        .and.resolveTo([
+          { id: 1, url: 'https://example.com', status: 'loading' },
+        ]);
+
+      await injectContentScripts();
+
+      expect(chromeMock.scripting.executeScript).not.toHaveBeenCalled();
+      expect(chromeMock.scripting.insertCSS).not.toHaveBeenCalled();
+    });
+
+    it('should not inject into chrome:// tabs', async () => {
+      chromeMock.tabs.query
+        .withArgs({ url: 'https://*.example.com/*', status: 'complete' })
+        .and.resolveTo([{ id: 1, url: 'chrome://extensions' }]);
+
+      await injectContentScripts();
+
+      expect(chromeMock.scripting.executeScript).not.toHaveBeenCalled();
+      expect(chromeMock.scripting.insertCSS).not.toHaveBeenCalled();
+    });
+
+    it('should not inject into about:blank tabs', async () => {
+      chromeMock.tabs.query
+        .withArgs({ url: 'https://*.example.com/*', status: 'complete' })
+        .and.resolveTo([{ id: 1, url: 'about:blank' }]);
+
+      await injectContentScripts();
+
+      expect(chromeMock.scripting.executeScript).not.toHaveBeenCalled();
+      expect(chromeMock.scripting.insertCSS).not.toHaveBeenCalled();
+    });
+
+    it('should honour host permissions', async () => {
+      chromeMock.tabs.query
+        .withArgs({ url: 'https://*.example.com/*' })
+        .and.resolveTo([{ id: 1, url: 'https://example.com' }]);
+      chromeMock.tabs.query
+        .withArgs({ url: 'https://*.example.com/*', status: 'complete' })
+        .and.resolveTo([{ id: 1, url: 'https://example.com' }]);
+
+      await injectContentScripts();
+
+      expect(chromeMock.scripting.executeScript).toHaveBeenCalledWith({
+        target: { tabId: 1, allFrames: false },
+        files: ['content.js'],
+        world: 'ISOLATED',
+      });
+    });
+
+    it('should honour exclude_matches', async () => {
+      chromeMock.tabs.query
+        .withArgs({ url: 'https://*.example.com/*' })
+        .and.resolveTo([{ id: 1, url: 'https://excluded.example.com' }]);
+      chromeMock.tabs.query
+        .withArgs({ url: 'https://excluded.example.com/*' })
+        .and.resolveTo([{ id: 1, url: 'https://excluded.example.com' }]);
+      chromeMock.tabs.query
+        .withArgs({ url: 'https://*.example.com/*', status: 'complete' })
+        .and.resolveTo([{ id: 1, url: 'https://excluded.example.com' }]);
+
+      await injectContentScripts();
+
+      expect(chromeMock.scripting.executeScript).not.toHaveBeenCalled();
+      expect(chromeMock.scripting.insertCSS).not.toHaveBeenCalled();
+    });
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,114 @@
-export function hello() {
-  console.log('Hello, world 2!');
+/** Gets all the tabs whose URLs match any of the provided URLs.
+ *
+ * This functions filters on tab status and excludes sensitive URLs.
+ */
+async function getMatchedTabIds(
+  matchUrls: string[],
+  status?: 'loading' | 'complete',
+): Promise<Set<number>> {
+  const tabsPromises = matchUrls.map((url) => {
+    return chrome.tabs.query({ url, status });
+  });
+  const tabIds = new Set<number>();
+  const tabsResults = await Promise.all(tabsPromises);
+  for (const tabs of tabsResults) {
+    for (const tab of tabs) {
+      if (
+        tab.url &&
+        !tab.url.startsWith('chrome://') &&
+        !tab.url.startsWith('chrome-extension://') &&
+        !tab.url.startsWith('about:blank')
+      ) {
+        tabIds.add(tab.id!);
+      }
+    }
+  }
+  return tabIds;
+}
+
+/** Injects content scripts and CSS into tabs
+ *
+ * This function honours the manifest `host_permissions` as well as
+ * `content_scripts` level `matches` and `exclude_matches` fields.
+ */
+export async function injectContentScripts() {
+  const manifest = chrome.runtime.getManifest() as chrome.runtime.ManifestV3;
+  const hostPermissions = manifest.host_permissions || [];
+  const contentScripts = manifest.content_scripts || [];
+
+  const hostPermissionTabsPromise = getMatchedTabIds(hostPermissions);
+
+  const contentScriptPromises = contentScripts.map(async (script) => {
+    const matchesTabsPromise = getMatchedTabIds(script.matches!, 'complete');
+    const excludeMatchesTabsPromise = script.exclude_matches
+      ? getMatchedTabIds(script.exclude_matches)
+      : Promise.resolve(new Set<number>());
+
+    const [matchesTabs, excludeMatchesTabs] = await Promise.all([
+      matchesTabsPromise,
+      excludeMatchesTabsPromise,
+    ]);
+
+    return { script, matchesTabs, excludeMatchesTabs };
+  });
+
+  const [hostPermissionTabs, resolvedContentScripts] = await Promise.all([
+    hostPermissionTabsPromise,
+    Promise.all(contentScriptPromises),
+  ]);
+
+  for (const {
+    script,
+    matchesTabs,
+    excludeMatchesTabs,
+  } of resolvedContentScripts) {
+    for (const tabId of matchesTabs) {
+      if (hostPermissionTabs.has(tabId) && !excludeMatchesTabs.has(tabId)) {
+        try {
+          if (script.css) {
+            await chrome.scripting.insertCSS({
+              target: { tabId, allFrames: script.all_frames || false },
+              files: script.css,
+            });
+          }
+          if (script.js) {
+            await chrome.scripting.executeScript({
+              target: { tabId, allFrames: script.all_frames || false },
+              files: script.js,
+              world: script.world || 'ISOLATED',
+            });
+          }
+        } catch (error) {
+          const tab = await chrome.tabs.get(tabId);
+          console.error(
+            'Failed to inject content script into tab:',
+            tab,
+            error,
+          );
+        }
+      }
+    }
+  }
+}
+
+let ACTIVE_CB: (() => Promise<void>) | null = null;
+
+/** Registers handler to inject content scripts on extension loading. */
+export function register() {
+  if (ACTIVE_CB !== null) {
+    throw new Error('Already registered content script injection');
+  }
+  ACTIVE_CB = async () => {
+    await injectContentScripts();
+  };
+  self.addEventListener('activate', ACTIVE_CB);
+}
+
+/** Unregisters handler to inject content scripts on extension loading. */
+export function unregister() {
+  if (ACTIVE_CB === null) {
+    throw new Error('Not registered content script injection');
+  }
+  self.removeEventListener('activate', ACTIVE_CB!);
+  ACTIVE_CB = null;
 }
